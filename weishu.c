90f80262398055b2d98edb8015dd78f11e908f04
ZCB <jyzx290921@qq.com>
2026-06-06 16:54:25 +0800

    添加了注释

--- weishu.c
+++ weishu.c
@@ -6,5 +6,9 @@ int main() {
     printf("int 的大小: %zu 字节\n", sizeof(int));
     printf("long 的大小: %zu 字节\n", sizeof(long));
     printf("指针的大小: %zu 字节\n", sizeof(void*));
+//fjklsdhghiurfdjh;kasdfhjk
+    //时间到了发卡机哦功夫啊呸哦i人分案
+    ///soiadfhlouakhfkjl;jh
+    
     return 0;
 }
